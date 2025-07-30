a8e52ffe9972d22ed084f9da549e7ec9ee070f4a
vscerlet <vscerlet@gmail.com>
2025-07-30 21:01:07 +0300

    Moving the base package to a separate file packet.go

--- core.go
+++ core.go
@@ -9,22 +9,6 @@ import (
 	"net"
 )
 
-// basePacket represents the basic structure of a data packet,
-// consisting of a packet identifier (ID) and payload.
-// The ID is stored as a 32-bit integer, and the payload contains the rest of the packet.
-type basePacket struct {
-	ID      uint32
-	Payload []byte
-}
-
-// Parses the package into fields according to its structure
-func parseBasePacket(pkg []byte) *basePacket {
-	pkgID := binary.BigEndian.Uint32(pkg[:4])
-	payload := pkg[4:]
-
-	return &basePacket{ID: pkgID, Payload: payload}
-}
-
 type HandlerFunc func(conn net.Conn, payload []byte) error
 
 type Server struct {
