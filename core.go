e5026e63dd941da2b31f5e4e0879235518198b17
vscerlet <vscerlet@gmail.com>
2025-07-30 09:49:19 +0300

    Creating a separate basePacket structure and transferring the parsing function to it

--- core.go
+++ core.go
@@ -9,6 +9,22 @@ import (
 	"net"
 )
 
+// basePacket represents the basic structure of a data packet,
+// consisting of a packet identifier (ID) and payload.
+// The ID is stored as a 32-bit integer, and the payload contains the rest of the packet.
+type basePacket struct {
+	ID      uint32
+	Payload []byte
+}
+
+// Parses the package into fields according to its structure
+func parseBasePacket(pkg []byte) *basePacket {
+	pkgID := binary.BigEndian.Uint32(pkg[:4])
+	payload := pkg[4:]
+
+	return &basePacket{ID: pkgID, Payload: payload}
+}
+
 type HandlerFunc func(conn net.Conn, payload []byte) error
 
 type Server struct {
@@ -81,21 +97,13 @@ func (s *Server) handleConnection(conn net.Conn) {
 		}
 
 		// Unpacking the package
-		pkgID, payload := parsePkg(buffer[:n])
+		pkg := parseBasePacket(buffer[:n])
 
-		handler, ok := s.handlers[pkgID]
+		handler, ok := s.handlers[pkg.ID]
 		if !ok {
-			log.Printf("Получен пакет с неизвестный ID - %d", pkgID)
+			log.Printf("Получен пакет с неизвестный ID - %d", pkg.ID)
 			continue
 		}
-		go handler(conn, payload)
+		go handler(conn, pkg.Payload)
 	}
-}
-
-// Parses the package into fields according to its structure
-func parsePkg(pkg []byte) (uint32, []byte) {
-	pkgID := binary.BigEndian.Uint32(pkg[:4])
-	payload := pkg[4:]
-
-	return pkgID, payload
 }
